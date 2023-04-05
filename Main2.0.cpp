f368c0698bf2ed8ba9a77822a36c3a5eb9d55309
Anastasia <cloverfield1408@gmail.com>
2023-04-05 23:39:43 +0300

    Изменила язык на русский

---
+++ Main2.0.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <Windows.h>
+#include "Chat2.0.h"
+
+int main() 
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Chat chat; // чат создан
+
+	chat.start(); 
+
+	while (chat.chatWork())
+	{
+		chat.showMainMenu();
+		while (chat.getCurrentUser())
+		{
+			chat.showChatMenu();
+		}
+	}
+	
+	return 0;
+}
